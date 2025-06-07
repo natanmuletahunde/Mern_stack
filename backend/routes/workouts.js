d4d71babddf6d75bf38942eb6bb04ae5dccdd7ef
pivnati <natanmuleta911@gmail.com>
2025-06-07 09:27:08 -0700

    modfiy the name  file in  the model  folder

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Workout = require('../models/workoutModel');
 const router = express.Router();
 
 
@@ -8,7 +9,14 @@ router.get('/', (req,res)=>{
 router.get('/:id', (req,res)=>{
     res.json({message: 'Get a single  workouts'});  //  returning a JSON response with a message
 })
-router.post('/', (req,res)=>{
+router.post('/',  async(req,res)=>{
+   const {title, reps, load} = req.body;
+     try {
+        const workout = await Workout.create({title, reps, load});  //  creating a new workout using the create method from the workoutModel
+      res.status(200).json(workout) //  returning the created workout as a JSON response 
+     } catch (error) {
+        res.status(400).json({error: error.message});  //  returning an error message if the workout creation fails
+     }
     res.json({message: 'Create a new workout'});  //  returning a JSON response with a message
 })
 router.patch('/:id', (req,res)=>{
