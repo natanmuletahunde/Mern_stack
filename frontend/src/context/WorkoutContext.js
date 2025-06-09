07647f00624cfe84435366be8950aca94b055cf6
pivnati <natanmuleta911@gmail.com>
2025-06-09 02:38:21 -0700

    create   the reactcontext

---
+++ frontend/src/context/WorkoutContext.js
@@ -0,0 +1,9 @@
+import {createContext} from 'react';
+export const WorkoutContext = createContext();
+export const WorkoutContextProvider = ({children}) => {
+  return (
+    <WorkoutContext.Provider value={{}}>
+      {children}
+    </WorkoutContext.Provider>
+  );
+};
