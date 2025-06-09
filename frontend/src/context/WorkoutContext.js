b60b577664fcba26b5e63548af2bf98e232353f1
pivnati <natanmuleta911@gmail.com>
2025-06-09 05:52:42 -0700

    create    hooks folder inside it create the workoutcontext useworkoutscontext

--- frontend/src/context/WorkoutContext.js
+++
@@ -1,9 +0,0 @@
-import {createContext} from 'react';
-export const WorkoutContext = createContext();
-export const WorkoutContextProvider = ({children}) => {
-  return (
-    <WorkoutContext.Provider value={{}}>
-      {children}
-    </WorkoutContext.Provider>
-  );
-};
