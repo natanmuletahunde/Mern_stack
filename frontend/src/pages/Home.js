c739d32e1ee79eb88d8fa9e6e8081e7b70991b2e
pivnati <natanmuleta911@gmail.com>
2025-06-09 01:22:39 -0700

    create the workoutdetailes and connect the fronend with the backend

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import { useEffect, useState } from "react"
+
+// components
+import WorkoutDetails from "../components/WorkoutDetails"
 
 const Home = () => {
+  const [workouts, setWorkouts] = useState(null)
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch('/api/workouts')
+      const json = await response.json()
+
+      if (response.ok) {
+        setWorkouts(json)
+      }
+    }
+
+    fetchWorkouts()
+  }, [])
+
   return (
-    <div className='home'>
-            <h1>
-                Home
-            </h1>
+    <div className="home">
+      <div className="workouts">
+        {workouts && workouts.map(workout => (
+          <WorkoutDetails workout={workout} key={workout._id} />
+        ))}
+      </div>
     </div>
   )
 }
