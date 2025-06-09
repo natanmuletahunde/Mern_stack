47759cc6cd124d404958087373ad33bbc7f35af1
pivnati <natanmuleta911@gmail.com>
2025-06-09 00:55:47 -0700

    create the navbar and modfiy the home.js pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import {BrowserRouter , Routes, Route } from 'react-router-dom'
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+
 function App() {
   return (
     <div className="App">
     <BrowserRouter>
-      <div>
+      <Navbar/>
+      <div className='pages'>
         <Routes>
           <Route path='/'
-           element={}
+           element={<Home/>}
            />
         </Routes>
       </div>
