d2d84a23576b8f393a3a77f66ae8e012112d4433
pivnati <natanmuleta911@gmail.com>
2025-06-11 16:56:07 -0700

    create the usesignup.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { WorkoutsContextProvider } from './context/WorkoutContext'
+import { WorkoutsContextProvider } from './context/WorkoutsContext'
 import { AuthContextProvider } from './context/AuthContext'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -7,7 +7,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault()
 
-    console.log(email, password)
+    console.log(email, password) 
   }
 
   return (
