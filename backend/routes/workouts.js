5a376489d06614dfe938296f055a70263869c596
pivnati <natanmuleta911@gmail.com>
2025-06-12 13:52:28 -0700

    create the middleware  for the workou routes

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,13 +1,32 @@
-const express = require('express');
-const { createWorkout, getWorkouts ,
-    getWorkout , deleteWorkout, updateWorkout
-} = require('../controllers/workoutController');
-const router = express.Router();
+const express = require('express')
+const {
+  createWorkout,
+  getWorkouts,
+  getWorkout,
+  deleteWorkout,
+  updateWorkout
+} = require('../controllers/workoutController')
+const requireAuth = require('../middleware/requireAuth')
+
+const router = express.Router()
+
+// require auth for all workout routes
+router.use(requireAuth)
+
+// GET all workouts
+router.get('/', getWorkouts)
+
+//GET a single workout
+router.get('/:id', getWorkout)
+
+// POST a new workout
+router.post('/', createWorkout)
+
+// DELETE a workout
+router.delete('/:id', deleteWorkout)
+
+// UPDATE a workout
+router.patch('/:id', updateWorkout)
 
 
-router.get('/',getWorkouts)
-router.get('/:id',getWorkout)
-router.post('/',createWorkout)
-router.patch('/:id',updateWorkout)  // patch means updated the database 
-router.delete('/:id',deleteWorkout)
-module.exports = router; 
+module.exports = router
