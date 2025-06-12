adea8280bf93a75ebff5e57d9a73209967c5e23e
pivnati <natanmuleta911@gmail.com>
2025-06-11 17:46:00 -0700

    useauthcontext.js

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -9,14 +9,12 @@ export const useSignup = () => {
   const signup = async (email, password) => {
     setIsLoading(true)
     setError(null)
-
     const response = await fetch('/api/user/signup', {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({ email, password })
     })
     const json = await response.json()
-
     if (!response.ok) {
       setIsLoading(false)
       setError(json.error)
